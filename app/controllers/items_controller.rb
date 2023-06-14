c698b63cc9c91bb321e0c714219c9096291b8fac
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-06-14 19:19:56 +0900

    views実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
+
   def index
-    
+   
+
   end
+#   def new
+
+#   end
+#   def create
+
+#   end
+#   def show
+
+#   end
+# end
+
+# private
+
+# def items_params
+#   params.require(:items).permit( :title).merge(user_id: current_user.id)
 end
