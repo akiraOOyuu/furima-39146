56d717e1e1baca71e039bbd0a34d88565fecdfd1
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-06-18 12:23:35 +0900

    エラーメッセージ表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,17 +13,18 @@ class Item < ApplicationRecord
 
   
   validates :title          ,presence: true
-  validates :content        ,presence: true
-  validates :price          ,presence: true
-  validates :user           ,presence: true
+  validates :content        ,presence: true 
+  validates :price          ,presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates_format_of       :price, with: /\A\d+\z/
+  validates :user_id        ,presence: true
   validates :image          ,presence: true, unless: :was_attached?
 
   # 選択しないとエラーメッセージを表示させる
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality:   { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality:     { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality:    { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :deli_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deli_day_id, numericality:   { other_than: 1 , message: "can't be blank"}
 
   def was_attached?
     self.image.attached?
