b7d1cf06a4fec65eb09eb04cc594a21b52c468cc
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-06-24 20:46:09 +0900

    payのviewとparams設定

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -4,6 +4,18 @@ class PaysController < ApplicationController
     @pays = Pay.new
   end
   def create
-    
+    @pay = Pay.new(pay_params)
+    if @pay.valid?
+      @pay.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def pay_params
+    params.require(:pay).permit(:price)
   end
 end
