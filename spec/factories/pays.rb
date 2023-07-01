7d7a8439139ccf03dd8e78095116032bed135cc4
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-01 12:57:39 +0900

    エラーハンドリング

--- spec/factories/pays.rb
+++ spec/factories/pays.rb
@@ -1,10 +1,3 @@
 FactoryBot.define do
-  factory :pay do
-     user_id {1}
-     item_id {1}
-     id {1}
-
-     association :user, factory: :user
-     association :item, factory: :item
-  end
+  
 end
--- spec/models/pay_spec.rb
+++ spec/models/pay_spec.rb
@@ -1,22 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Pay, type: :model do
-  before do
-    @pay = FactoryBot.build(:pay)
-  end
-
-  context '内容に問題ない場合' do
-    it "priceがあれば保存ができること" do
-     
-      expect(@pay).to be_valid
-    end
-  end
-
-  context '内容に問題がある場合' do
-    it "priceが空では保存ができないこと" do
-      @pay.price = nil
-      @pay.valid?
-      expect(@pay.errors.full_messages).to include("Price can't be blank")
-    end
-  end
+ 
 end
